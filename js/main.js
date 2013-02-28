bee7cbaef5231323768e2e82c57eb970d39d3154
apex2060 <apex2060@gmail.com>
2013-02-28 15:13:24 -0800

    Facebook javascript login added

--- js/main.js
+++ js/main.js
@@ -1,42 +1,77 @@
-$(document).ready(function() {
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : 'your_FB_app_ID', // FIXME
+    channelUrl : '//developer.localhost:4567/index.html', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+ 
+  // Additional initialization code here
+};
+ (function(d){
+   var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = '//connect.facebook.net/en_US/all.js';
+   d.getElementsByTagName('head')[0].appendChild(js);
+ }(document));
 
-// Define your class/object
-var TestObject = StackMob.Model.extend({
-schemaName : 'testobject' //Tell StackMob to save instances of TestObject to the table "testobject"
-});
 
-// Create a local instance of TestObject by passing JSON into the constructor
-// We have several different field types here as examples.
-// Fields must be lower case.
-var to = new TestObject({
-message : 'This test object instance will show you how StackMob creates your DB from JSON data',
-integer : 1,
-decimal : 2.5,
-istestobject : true,
-stringarray : ['This', 'is', 'an', 'array', 'of', 'strings'],
-intarray : [1, 2, 3, 4, 5],
-decimals : [1.1, 2.2, 3.3],
-booleans : [true, false, true, false]
-});
 
-// Call "create" to fire off the AJAX call to StackMob to create this object instance
-// If this is the first time StackMob has seen a 'testobject', StackMob will create the database schema automatically for you.
-// When the server is done, AJAX callbacks are fired: 'success' / 'error'
-to.create({
-success : function(model) {
-// Notice how StackMob auto generated a unique primary key for this object instance under "testobject_id".
-// Schema primary keys are [schema name]_id
-// If you specified "testobject_id" in your JSON, StackMob will use your given one instead.
-console.debug(model.toJSON());
-$(".error").hide();
-$(".success").show();
-},
-error : function(model, response) {
-console.debug("Oops there was an error in creating the object.");
-console.debug("Have you initialized your JS SDK?");
-console.debug("Are you running this on StackMob's Local Runner?");
-console.debug("Are you running this on StackMob's GitHub-integrated hosting?")
-console.debug(response);
+function saveGeoLoc(){
+	// If your schema name is different than `place`, then you need to replace `place` with your schema
+	var Place = StackMob.Model.extend({ schemaName: 'place' });
+	var Places = StackMob.Collection.extend({ model: Place });
+	 
+	// GeoPoint(lat, lon) has some constraints:
+	//  lat: -90,90 (both inclusive)
+	//  lon: -180 (inclusive), 180 (exclusive)
+	var latlon = new StackMob.GeoPoint(37.772161, -122.406443);
+	 
+	var q = new StackMob.Collection.Query();
+	q.mustBeNear('location', latlon, 1);
+	 
+	var placesContainer = new Places();
+	placesContainer.query(q, {
+	  success: function(model) {
+	    console.debug(model);
+	  },
+	  error: function(model, response) {
+	    console.debug(response);
+	  }
+	});
 }
-});
+
+
+
+
+
+
+
+function login() {
+	//Login with Facebook
+	FB.login(function(response) {
+		if (response.authResponse) {
+			var accessToken = response.authResponse.accessToken;
+
+			FB.api('/me', function(response) {
+				var user = new StackMob.User({ username: response.email });
+				user.createUserWithFacebook(accessToken, {
+					success: function(model) {
+						console.debug(model);
+					},
+					error: function(model, response) {
+						console.debug(model);
+						console.debug(response);
+					}
+				});
+			});
+		} else {
+			console.log('User canceled login or did not fully authorize.');
+		}
+	}, {scope: 'email'});
+}
+
+$('#login').click(function() {
+	login();
 });
