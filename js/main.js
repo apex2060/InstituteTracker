43d5f38271948cfe90ccd1018bed65104e4144ce
apex2060 <apex2060@gmail.com>
2013-02-28 18:27:21 -0800

    Added geo-location functionality

--- js/main.js
+++ js/main.js
@@ -1,45 +1,46 @@
 var it={};
-
-function saveGeoLoc(){
-	// If your schema name is different than `place`, then you need to replace `place` with your schema
-	var Place = StackMob.Model.extend({ schemaName: 'place' });
-	var Places = StackMob.Collection.extend({ model: Place });
-	 
-	// GeoPoint(lat, lon) has some constraints:
-	//  lat: -90,90 (both inclusive)
-	//  lon: -180 (inclusive), 180 (exclusive)
-	var latlon = new StackMob.GeoPoint(37.772161, -122.406443);
-	 
-	var q = new StackMob.Collection.Query();
-	q.mustBeNear('location', latlon, 1);
-	 
-	var placesContainer = new Places();
-	placesContainer.query(q, {
-	  success: function(model) {
-	    console.debug(model);
-	  },
-	  error: function(model, response) {
-	    console.debug(response);
-	  }
-	});
-}
-
-
-
-
+	it.error=[];
 
 function login() {
-	var user = new StackMob.User({ username: 'Ryan Quinlan', password: 'somecoolencryption', profession: 'developer'});
-	user.create({
-	    success: function(model) {
-	        console.debug('User object is saved, username: ' + model.get('username'));
-	        it=model;
-	    },
-	    error: function(model, response) {
-	        console.debug(response);
-	    }
-	});
+	if(it.currentLocation==undefined){
+		alert('Geo Location must be activated to use this.');
+		promptLocation();
+	}else{
+		var latlon = new StackMob.GeoPoint(position.coords.latitude, position.coords.longitude);
+		var user = new StackMob.User({ username: 'Ryan Quinlan', password: 'somecoolencryption', profession: 'developer', geoloc: latlon});
+		user.create({
+		    success: function(model) {
+		        console.debug('User object is saved, username: ' + model.get('username'));
+		        it=model;
+		    },
+		    error: function(model, response) {
+		        console.debug(response);
+		    }
+		});
+	}
 }
+
+function promptLocation(){
+	if (navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(setLocation);
+	}
+	else{
+		it.error.push("Geolocation is not supported by this browser.");
+	}
+}
+function setLocation(position){
+	it.currentLocation=position;
+}
+
+
+
+
+
+
+
+
+
+
 
 $('#login').click(function() {
 	login();
