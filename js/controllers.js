6592296a12e1b7a0f286cd4125c98dc35075b75a
apex2060 <apex2060@gmail.com>
2013-03-04 09:47:00 -0800

    Testing form submission with angular controllers

--- js/controllers.js
+++ js/controllers.js
@@ -5,3 +5,17 @@ function actCtrl($scope, $routeParams) {
 function mainCtrl($scope, $routParams){
 	$scope.it=it;
 }
+
+function formCtrl($scope, $routParams){
+	$scope.submit = function() {
+		console.log($scope);
+		console.log($routParams.toJSON());
+	};
+	
+	/*
+	var newObj = StackMob.Model.extend({ schemaName: 'todo' });
+	// completed will be a boolean field if it's not created already
+	var myTodo = new Todo({ title: 'Send Data to StackMob!', priority: 1, completed: true });
+	myTodo.create();
+	*/
+}
