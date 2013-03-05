5bedb88da2022c2a62ecbc1c350c6da81a5285ad
apex2060 <apex2060@gmail.com>
2013-03-04 22:44:10 -0800

    added hash redirect

--- js/controllers.js
+++ js/controllers.js
@@ -8,13 +8,15 @@ function formCtrl($scope, $element){
 		var form = angular.element($element);
 		it.form=$(form).serializeObject();
 		var schema = $(form).data('schema');
-
+		var hash = $(form).data('hash');
 		var obj = StackMob.Model.extend({ schemaName: schema });
 		// completed will be a boolean field if it's not created already
 		var newObj = new obj(it.form);
 		newObj.create({
 			success: function(model, response) {
 				it.message.wisper('alert-success', schema+' created successfully.');
+				if(hash)
+					window.location.hash=hash;
 			},
 			error: function(model, response) {
 				it.error.push(response);
