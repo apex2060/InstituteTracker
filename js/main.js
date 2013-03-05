8d0448a65e4c63e2b5180c32bcdf498f60dbbf92
apex2060 <apex2060@gmail.com>
2013-03-05 09:32:33 -0800

    try that with direct link to modal

--- js/main.js
+++ js/main.js
@@ -111,7 +111,7 @@ $('#formCreate').submit(function(e){
 	return false;
 });
 
-$('#loginModal').$el.on('shown', function () {
+$('#loginModal').on('shown', function () {
   $('input:text:visible:first', this).focus();
 });
 /**************************************** JQUERY FUNCTIONS ****************************************/
