d6613aca7101f7055c69020256723e2695ad8beb
apex2060 <apex2060@gmail.com>
2013-03-11 16:58:50 -0700

    testing

--- js/geolocation.js
+++ js/geolocation.js
@@ -4,40 +4,40 @@ var maps={
 	pendingMaps: 	[],
 	openMaps: 		[],
 	marker: 		[]
-};
+}
 
 maps.init = function(mapCanvasId, lat, lng){
-	var mapData={};
-		mapData.canvasId=mapCanvasId;
-		mapData.lat=lat;
-		mapData.lng=lng;
+	var mapData={}
+		mapData.canvasId=mapCanvasId
+		mapData.lat=lat
+		mapData.lng=lng
 
 	if(!this.scriptLoaded){
-		this.getScript();
-		this.pendingMaps.push(mapData);
+		this.getScript()
+		this.pendingMaps.push(mapData)
 	}else{
-		this.newMap(mapData);
+		this.newMap(mapData)
 	}
 }
 
 maps.getScript = function(){
 	if(!this.scriptLoaded){
 		if(!scriptLoading){
-			scriptLoading=true;
-			var script = document.createElement("script");
-			script.type = "text/javascript";
-			script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAw0i5KC7opeWmEF4jE6oYWu0UxjTOINj4&sensor=false&callback=maps.scriptLoaded";
-			document.body.appendChild(script);
+			scriptLoading=true
+			var script = document.createElement("script")
+			script.type = "text/javascript"
+			script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyAw0i5KC7opeWmEF4jE6oYWu0UxjTOINj4&sensor=false&callback=maps.scriptLoaded"
+			document.body.appendChild(script)
 		}
 	}
 }
 
 maps.scriptLoaded = function(){
-	this.scriptLoaded	= true;
-	this.scriptLoading	= false;
-	this.geocoder 		= new google.maps.Geocoder();
-	for(var i=0; i<this.pendingMaps.length; i++){
-		this.newMap(this.pendingMaps[i]);
+	this.scriptLoaded	= true
+	this.scriptLoading	= false
+	this.geocoder 		= new google.maps.Geocoder()
+	for(var i=0 i<this.pendingMaps.length i++){
+		this.newMap(this.pendingMaps[i])
 	}
 }
 
@@ -47,37 +47,37 @@ maps.newMap = function(mapData){
 			center: new google.maps.LatLng(mapData.lat,mapData.lng),
 			zoom: 14,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
-		this.openMaps[this.openMaps.length] = new google.maps.Map(document.getElementById(mapData.canvasId), mapOptions);
+		}
+		this.openMaps[this.openMaps.length] = new google.maps.Map(document.getElementById(mapData.canvasId), mapOptions)
 	}
 }
 
 maps.setFromAddress = function(map, address){
     this.geocoder.geocode( { 'address': address}, function(results, status) {
 	    if (status == google.maps.GeocoderStatus.OK) {
-	    	var geo = results[0].geometry.location;
+	    	var geo = results[0].geometry.location
 
-	    	mapData.lat=geo.ib;
-	    	mapData.lng=geo.jb;
+	    	mapData.lat=geo.ib
+	    	mapData.lng=geo.jb
 
-	        map.setCenter(geo, 16);
+	        map.setCenter(geo, 16)
 
 	        this.marker[this.marker.length] = new google.maps.Marker({
 	        	map: map, 
 	        	position: geo
-	        });
+	        })
 
-	        this.marker[this.marker.length].setDraggable (true);
-	        google.maps.event.addListener(this.marker[this.marker.length], "dragend", maps.markerMove);
+	        this.marker[this.marker.length].setDraggable (true)
+	        google.maps.event.addListener(this.marker[this.marker.length], "dragend", maps.markerMove)
 	    }
-	});
+	})
 }
 
 maps.markerMove = function(event){
-	console.log(event);
-	//it.point = it.marker.getPosition();
-	//it.map.panTo(it.point);
-	//console.log(it.point);
+	console.log(event)
+	//it.point = it.marker.getPosition()
+	//it.map.panTo(it.point)
+	//console.log(it.point)
 }
 
 function parseContact(str){
