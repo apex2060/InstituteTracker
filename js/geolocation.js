695cb38162fb997d5d9465ff445eface6cad3e11
apex2060 <apex2060@gmail.com>
2013-03-12 08:49:38 -0700

    testing

--- js/geolocation.js
+++ js/geolocation.js
@@ -69,14 +69,14 @@ maps.setFromAddress = function(canvasId, address, dragable){
 			maps.openMaps[canvasId].setCenter(myLatLng);
 			if(maps.marker[canvasId]==undefined)
 				maps.marker[canvasId] = [];
-			maps.marker[canvasId][maps.marker[canvasId].length] = new google.maps.Marker({
+			maps.marker = new google.maps.Marker({
 					map: maps.openMaps[canvasId], 
 					position: geo
 				})
 
-			maps.marker[canvasId][maps.marker[canvasId].length-1].setDraggable (true);
-			google.maps.event.addListener(maps.marker[canvasId][maps.marker[canvasId].length-1], "dragend", function(event){
-				maps.markerMove(event, canvasId, maps.marker[canvasId].length-1);
+			maps.marker.setDraggable (true);
+			google.maps.event.addListener(maps.marker, "dragend", function(event){
+				maps.markerMove(event, canvasId);
 			});
 		}
 	});
